a8e6cf3c700bcac3be32fb7e718df4e5abccb263
Mountain Cat <tomasz.banas123@gmail.com>
2023-08-27 20:49:12 +0200

    add building set

---
+++ Realm/Assets/Scripts/Data/BuildingMetadataSet.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Linq;
+using Data;
+using UnityEditor;
+using UnityEngine;
+
+namespace Data
+{
+    [CreateAssetMenu(menuName = "Data/Building Set", fileName = nameof(BuildingMetadataSet))]
+    public class BuildingMetadataSet : ScriptableObject
+    {
+        [field: SerializeField] public BuildingMetadata[] Buildings { get; set; }
+    }
+}
+
