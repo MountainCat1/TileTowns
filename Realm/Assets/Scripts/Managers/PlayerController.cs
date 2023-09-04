97f4307f1d1970eb4ca58f7c43613d9e64b9f052
Mountain Cat <tomasz.banas123@gmail.com>
2023-09-04 17:45:09 +0200

    added tab action to InputManager.cs

---
+++ Realm/Assets/Scripts/Managers/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using Zenject;
+
+public enum PlayerMode
+{
+    Default, Building, PopulationManaging
+}
+
+public class PlayerController : MonoBehaviour
+{
+    [Inject] private IInputManager _inputManager;
+
+    private void OnEnable()
+    {
+        _inputManager.PlayerPressedTab += SwitchBetweenModes;
+    }
+
+    private void SwitchBetweenModes()
+    {
+        // TODO
+        Debug.LogError("AAA this is NOT implemented! WHY?!!!!! 💀💀💀");
+    }
+}
