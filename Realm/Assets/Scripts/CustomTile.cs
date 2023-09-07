aa992d62b217c6b13725111fcf1a63266643ad63
Mountain Cat <tomasz.banas123@gmail.com>
2023-09-07 02:19:28 +0200

    changed project directory name

--- Realm/Assets/Scripts/CustomTile.cs
+++
@@ -1,5 +0,0 @@
-using UnityEngine.Tilemaps;
-
-public class CustomTile : Tile
-{
-}
--- Realm/Assets/Scripts/LevelDescriptor.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-using UnityEngine.Tilemaps;
-
-public class LevelDescriptor : MonoBehaviour
-{
-    [field: SerializeField] public Tilemap Map { get; set; }
-}
