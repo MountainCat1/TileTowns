086435e2a61a40eea957f3b64f0e3ef9091af45d
Mountain Cat <tomasz.banas123@gmail.com>
2023-09-08 16:15:26 +0200

    optimized updating display in ImmigrationPanelUI.cs

---
+++ TileTowns/Assets/Scripts/UI/ImmigrationPanelUI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace UI
+{
+    public class ImmigrationPanelUI : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI changeDisplay;
+        [SerializeField] private TextMeshProUGUI progressTextDisplay;
+        [SerializeField] private Slider progressSliderDisplay;
+
+        [Inject] private IGameState _gameState;
+
+        [Inject]
+        void Construct()
+        {
+            _gameState.Changed += UpdateData;
+            _gameState.MutationChanged += UpdateMutationData;
+        }
+
+        private void UpdateMutationData()
+        {
+            var immigrationChange = _gameState.Mutations.Sum(x => x.ImmigrationChange);
+            
+            changeDisplay.text = $"+ {immigrationChange}%";
+        }
+
+        private void UpdateData()
+        {
+            progressSliderDisplay.value = _gameState.Immigration;
+            progressTextDisplay.text = $"{_gameState.Immigration}%";
+        }
+    }
+}
