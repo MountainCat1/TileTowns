4414ff3d6550a19704295cddcce89ca3d456f2ca
Mountain Cat <tomasz.banas123@gmail.com>
2023-09-10 12:57:50 +0200

    added panel for displaying population info

---
+++ TileTowns/Assets/Scripts/UI/PopulationDisplayUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PopulationDisplayUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI populationDisplay;
+    [SerializeField] private TextMeshProUGUI workslotsDisplay;
+    [SerializeField] private TextMeshProUGUI housingDisplay;
+}
