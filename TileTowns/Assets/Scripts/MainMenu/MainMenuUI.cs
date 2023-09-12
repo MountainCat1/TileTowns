7f1ec1d7cbba43d6ac20fc921da7eab4c0d30f02
Bartek Banaś <bartek.banas675@gmail.com>
2023-09-12 20:29:01 +0200

    Add basic menu functionality

---
+++ TileTowns/Assets/Scripts/MainMenu/MainMenuUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+ 
+namespace MainMenu
+{
+    public class MainMenuUI : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> pages;
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+        
+        public void ShowPage(string pageName)
+        {
+            var targetPage = pages.First(x => x.name == pageName);
+
+            foreach (var page in pages)
+            {
+                page.SetActive(false);
+            }
+            
+            targetPage.SetActive(true);
+        }
+    }
+}
