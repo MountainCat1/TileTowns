869f59f75e3659e903488c7ced4ff7a0dd0f614f
Bartek Banaś <bartek.banas675@gmail.com>
2023-09-15 10:23:41 +0200

    Add Church script

---
+++ TileTowns/Assets/Scripts/Buildings/Church.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Buildings
+{
+    public class Church : Building
+    {
+        [Inject] private ITileMapData _mapData;
+
+        public override int WorkSlots => 2;
+        public override int Housing => 2;
+
+        [field: SerializeField] public float CostPerWorker { get; set; }
+
+        public override void UpdateMutation(ITileData tileData, IGameStateTurnMutation mutation)
+        {
+            mutation.BuildingIncome = -CostPerWorker * tileData.WorkersAssigned;
+            mutation.ImmigrationChange = 50 * tileData.WorkersAssigned;
+        }
+    }
+}
