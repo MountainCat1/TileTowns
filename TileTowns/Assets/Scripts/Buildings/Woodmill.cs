a09f852bbb1bfb6368d49b6775342f2d5d76aa3d
Bartek Banaś <bartek.banas675@gmail.com>
2023-09-15 10:32:27 +0200

    Add Woodmill script

---
+++ TileTowns/Assets/Scripts/Buildings/Woodmill.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Buildings
+{
+    public class Woodmill : Building
+    {
+        [Inject] private ITileMapData _mapData;
+        
+        public override int WorkSlots => 4;
+        public override int Housing => 0;
+
+        [field: SerializeField] public float MoneyPerWorker { get; set; }
+        
+        public override void UpdateMutation(ITileData tileData, IGameStateTurnMutation mutation)
+        {
+            mutation.BuildingIncome = MoneyPerWorker * tileData.WorkersAssigned;
+        }
+    }
+}
