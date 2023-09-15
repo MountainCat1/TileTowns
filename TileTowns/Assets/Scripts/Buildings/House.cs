e8cd0ee19473f6f910b3528f62f3c27032929343
Bartek Banaś <bartek.banas675@gmail.com>
2023-09-15 10:23:55 +0200

    Add House script

---
+++ TileTowns/Assets/Scripts/Buildings/House.cs
@@ -0,0 +1,17 @@
+﻿using Zenject;
+
+namespace Buildings
+{
+    public class House : Building
+    {
+        [Inject] private ITileMapData _mapData;
+        
+        public override int WorkSlots => 0;
+        public override int Housing => 8;
+
+        public override void UpdateMutation(ITileData tileData, IGameStateTurnMutation mutation)
+        {
+            mutation.ImmigrationChange = 5 * Housing;
+        }
+    }
+}
