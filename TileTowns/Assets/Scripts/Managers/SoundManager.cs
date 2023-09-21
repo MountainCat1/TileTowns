1b89f5a49d3db2695359dfd12aee9be5ebfd6be4
Mountain Cat <tomasz.banas123@gmail.com>
2023-09-21 13:50:52 +0200

    added sound types

--- TileTowns/Assets/Scripts/Managers/SoundManager.cs
+++ TileTowns/Assets/Scripts/Managers/SoundManager.cs
@@ -1,6 +1,15 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UI;
+using UnityEngine;
 using Zenject;
 using Object = UnityEngine.Object;
+
+public enum SoundType
+{
+    Sfx,
+    Music
+}
 
 public interface ISoundManager
 {
@@ -15,14 +24,20 @@ public class SoundManager : ISoundManager
     [Inject] private IBuildingController _buildingController;
     [Inject] private ITurnManager _turnManager;
     [Inject] private IGameManager _gameManager;
+    [Inject] private PopulationController _tileMapData;
     
     [Inject] private Camera _camera;
 
     private Transform soundParent;
+    
+    private readonly Dictionary<SoundType, IList<AudioSource>> _audioSources = new();
 
     [Inject]
     private void Construct()
     {
+        _audioSources[SoundType.Music] = new List<AudioSource>();
+        _audioSources[SoundType.Sfx] = new List<AudioSource>();
+        
         soundParent = _camera.transform;
         
         _buildingController.PlaceBuildingFailed += delegate { PlaySound(_gameSounds.ErrorSound); };
@@ -30,12 +45,23 @@ public class SoundManager : ISoundManager
         
         _turnManager.TurnEnded += delegate { PlaySound(_gameSounds.TurnEnded); };
         
-        _gameManager.LevelLoaded += delegate { PlaySound(_gameSounds.GameMusic); };
+        _gameManager.LevelLoaded += delegate { PlaySound(_gameSounds.GameMusic, SoundType.Music); };
+        
+        _tileMapData.PopAssigned += delegate { PlaySound(_gameSounds.GameMusic, SoundType.Music); };
     }
 
-    private void PlaySound(AudioClip clip)
+    private void PlaySound(AudioClip clip, SoundType soundType = SoundType.Sfx)
     {
-        PlayAtPoint(clip, soundParent);
+        var audioSource = PlayAtPoint(clip, soundParent);
+        _audioSources[soundType].Add(audioSource);
+    }
+
+    public void ChangeVolume(SoundType soundType, float targetVolume)
+    {
+        foreach (var audioSource in _audioSources[soundType])
+        {
+            audioSource.volume = targetVolume;
+        }
     }
 
     public static AudioSource PlayAtPoint(AudioClip clip, Transform parent, float volume = 1f)
