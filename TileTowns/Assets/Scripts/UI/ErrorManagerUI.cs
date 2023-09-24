b09c36ffb6bee07bde8485d8567a13bc5ad88b0c
Mountain Cat <tomasz.banas123@gmail.com>
2023-09-24 12:47:08 +0200

    added error console

---
+++ TileTowns/Assets/Scripts/UI/ErrorManagerUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace UI
+{
+    public class ErrorManagerUI : MonoBehaviour
+    {
+        [SerializeField] private ErrorEntryUI errorEntryPrefab;
+        [SerializeField] private Transform errorEntryContainer;
+
+        [SerializeField] private float timeToCleanEntry = 15f;
+
+        [Inject]
+        private void Construct(
+            IBuildingController buildingController
+        )
+        {
+            buildingController.PlaceBuildingFailed += () => LogError("Can't place building"); 
+        }
+
+
+        public void LogError(string message)
+        {
+            ErrorEntryUI entry = Instantiate(errorEntryPrefab, errorEntryContainer);
+
+            entry.SetError(message);
+            
+            Destroy(entry.gameObject, timeToCleanEntry);
+        }
+    }
+}
