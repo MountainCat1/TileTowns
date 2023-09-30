d6502841b52cb6b81a99754dec4dd28b2e0dd506
Mountain Cat <tomasz.banas123@gmail.com>
2023-09-30 15:11:46 +0200

    added initial immigration setting

--- TileTowns/Assets/Scripts/Data/ImmigrationSettings.cs
+++ TileTowns/Assets/Scripts/Data/ImmigrationSettings.cs
@@ -5,6 +5,8 @@ namespace Data
     [System.Serializable]
     public class ImmigrationSettings
     {
+        [field: SerializeField] public float InitialImmigration { get; set; } = 50f;
+        
         [field: SerializeField] public float ImmigrationPerPopulation { get; set; } = 100f;
         [field: SerializeField] public float ImmigrationForPopulation { get; set; } = -0.1f;
         [field: SerializeField] public float ImmigrationForFreeHousing { get; set; } = 0.2f;
