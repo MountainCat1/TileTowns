cb6c0b25b4bd284cdb9acfa7914768b0087680e6
Mountain Cat <tomasz.banas123@gmail.com>
2023-09-30 15:33:40 +0200

    added farm mechanic

--- TileTowns/Assets/Scripts/Buildings/Farm.cs
+++ TileTowns/Assets/Scripts/Buildings/Farm.cs
@@ -1,19 +1,24 @@
-﻿using Zenject;
+﻿using System.Linq;
+using UnityEngine;
 
 namespace Buildings
 {
     public class Farm : Building
     {
-        [Inject] private ITileMapData _mapData;
-        
         public override int WorkSlots => 4;
         public override int Housing => WorkSlots / 2;
 
-        public float moneyPerWorker;
+
+        [field: SerializeField] public float MoneyPerWorker { get; set; }
+        [field: SerializeField] public float ModifierPerAdjectedFarm { get; set; }
 
         public override void UpdateMutation(ITileData tileData, IGameStateTurnMutation mutation)
         {
-            mutation.BuildingIncome = moneyPerWorker * tileData.WorkersAssigned;
+            var modifier = 1 + GetAdjacentTiles(tileData.Position)
+                .Count(x => x.Building is Farm) 
+                * ModifierPerAdjectedFarm;
+            
+            mutation.BuildingIncome = MoneyPerWorker * tileData.WorkersAssigned * modifier;
         }
     }
 }
