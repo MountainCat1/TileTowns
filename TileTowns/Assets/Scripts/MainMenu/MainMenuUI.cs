7ad2aa94325c7d0515eb9677fbf295aa7adf05a7
Mountain Cat <tomasz.banas123@gmail.com>
2023-10-02 14:30:06 +0200

    re arranged main menu, added level selection view

--- TileTowns/Assets/Scripts/MainMenu/MainMenuUI.cs
+++ TileTowns/Assets/Scripts/MainMenu/MainMenuUI.cs
@@ -1,28 +1,43 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
- 
+using UnityEngine.Serialization;
+
 namespace MainMenu
 {
     public class MainMenuUI : MonoBehaviour
     {
-        [SerializeField] private List<GameObject> pages;
+        [SerializeField] private Transform viewsContainer;
+        [SerializeField] private MenuViewUI defaultView;
+        private List<MenuViewUI> _views;
 
+        private void Start()
+        {
+            _views = viewsContainer.GetComponentsInChildren<MenuViewUI>().ToList();
+            
+            ShowPage(defaultView);
+        }
+
+        public void ShowPage(MenuViewUI targetPage)
+        {
+            foreach (var page in _views)
+            {
+                page.gameObject.SetActive(false);
+            }
+            
+            targetPage.gameObject.SetActive(true);
+        }
+        public void ShowPage(string pageName)
+        {
+            var targetPage = _views.First(x => x.name == pageName);
+
+            ShowPage(targetPage);
+        }   
+        
         public void Quit()
         {
             Application.Quit();
-        }
-        
-        public void ShowPage(string pageName)
-        {
-            var targetPage = pages.First(x => x.name == pageName);
-
-            foreach (var page in pages)
-            {
-                page.SetActive(false);
-            }
-            
-            targetPage.SetActive(true);
         }
     }
 }
