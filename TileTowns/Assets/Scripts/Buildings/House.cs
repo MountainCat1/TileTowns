a759d8da84bcbaafcb34e6ce64110fa8da266864
Mountain Cat <tomasz.banas123@gmail.com>
2023-10-05 10:31:23 +0200

    Updated house

--- TileTowns/Assets/Scripts/Buildings/House.cs
+++ TileTowns/Assets/Scripts/Buildings/House.cs
@@ -1,13 +1,16 @@
-﻿using Zenject;
+﻿using UnityEngine;
+using Zenject;
 
 namespace Buildings
 {
     public class House : Building
     {
         [Inject] private ITileMapData _mapData;
+
+        [field: SerializeField] private int housingProvided;
         
         public override int WorkSlots => 0;
-        public override int Housing => 8;
+        public override int Housing => housingProvided;
         public override int ImmigrationChange => Housing / 2;
 
         public override void UpdateMutation(ITileData tileData, IGameStateTurnMutation mutation)
