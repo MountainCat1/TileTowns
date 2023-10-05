ada24c96faa122e698be559336cc7bab63b36360
Mountain Cat <tomasz.banas123@gmail.com>
2023-10-05 10:31:13 +0200

    updated church

--- TileTowns/Assets/Scripts/Buildings/Church.cs
+++ TileTowns/Assets/Scripts/Buildings/Church.cs
@@ -1,4 +1,6 @@
-﻿using Zenject;
+﻿using System.Linq;
+using UnityEngine;
+using Zenject;
 
 namespace Buildings
 {
@@ -9,10 +11,24 @@ namespace Buildings
         public override int WorkSlots => 2;
         public override int Housing => 2;
         public override int ImmigrationChange => 10;
-
+        
+        [field: SerializeField] public float MoneyPerHouse { get; set; }
+        
         public override void UpdateMutation(ITileData tileData, IGameStateTurnMutation mutation)
         {
+            var adjectedTiles = CountAdjacentHouseTiles(tileData);
+            
             mutation.ImmigrationChange = ImmigrationChange * (1 / 1 + WorkSlots - tileData.WorkersAssigned);
+            
+            mutation.BuildingIncome = adjectedTiles * tileData.WorkersAssigned * MoneyPerHouse;
+        }
+        
+        private int CountAdjacentHouseTiles(ITileData tileData)
+        {
+            var adjacentTiles = GetAdjacentTiles(tileData.Position);
+            int amountOfAdjacentForests = adjacentTiles.Count(tile => tile.Building is House);
+
+            return amountOfAdjacentForests;
         }
     }
 }
