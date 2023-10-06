3df17fbe9a8805697acbb2dce357e98e2709461c
Mountain Cat <tomasz.banas123@gmail.com>
2023-10-06 21:23:15 +0200

    added popup manager

---
+++ TileTowns/Assets/Scripts/Managers/PopupManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public interface IPopupManager
+{
+    void SpawnPopup(string text, Vector3 position, Color color);
+}
+
+public class PopupManager : MonoBehaviour, IPopupManager
+{
+    [SerializeField] private Transform popupContainer;
+    [SerializeField] private PopupDescriptor popupPrefab;
+    
+    public void SpawnPopup(string text, Vector3 position, Color color)
+    {
+        var popup = Instantiate(popupPrefab, popupContainer);
+        
+        popup.transform.position = position;
+        popup.Text.text = text;
+        popup.Text.color = color;
+    }
+}
---
+++ TileTowns/Assets/Scripts/UI/PopupDescriptor.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PopupDescriptor : MonoBehaviour
+{
+    [field: SerializeField] public TextMeshProUGUI Text { get; set; }
+}
