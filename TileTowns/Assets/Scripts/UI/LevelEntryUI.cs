1bb7ba0f1dff0b6fb8acd36780e1694136b3ceec
Mountain Cat <tomasz.banas123@gmail.com>
2023-10-06 23:16:32 +0200

    added progress indication

--- TileTowns/Assets/Scripts/UI/LevelEntryUI.cs
+++ TileTowns/Assets/Scripts/UI/LevelEntryUI.cs
@@ -14,16 +14,22 @@ public class LevelEntryUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI levelNameText;
     [SerializeField] private TextMeshProUGUI levelDescriptionText;
     [SerializeField] private Image levelImage;
+    [SerializeField] private Image lockImage;
+    [SerializeField] private Color lockedColor;
 
     private LevelConfig _levelConfig;
 
-    public void Initialize(LevelConfig levelConfig)
+    public void Initialize(LevelConfig levelConfig, bool unlocked)
     {
         _levelConfig = levelConfig;
         
         levelNameText.text = levelConfig.LevelName;
         levelDescriptionText.text = levelConfig.LevelDescription;
         levelImage.sprite = levelConfig.LevelImage;
+        
+        lockImage.gameObject.SetActive(!unlocked);
+        if(!unlocked)
+            levelImage.color = lockedColor;
     }
 
     public void Select()
