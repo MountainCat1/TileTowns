b7bef2d8ccc973a76652cf0bed42a91d24969ae2
Mountain Cat <tomasz.banas123@gmail.com>
2023-10-10 01:31:03 +0200

    added GetPopulationWinConditions.cs

---
+++ TileTowns/Assets/Scripts/Data/WinConditions/GetPopulationWinConditions.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Data.WinConditions
+{
+    [CreateAssetMenu(menuName = "Data/Win Conditions/Get Population")]
+    public class GetPopulationWinConditions : WinCondition
+    {
+        [field: SerializeField] public float Goal { get; set; }
+        [field: SerializeField] public int TurnLimit { get; set; }
+
+        public override string WinDescription => $"Reach {Goal}$ population";
+        public override string LoseDescription => $"Don't take more than {TurnLimit} turns";
+
+        public override IGameResult Check(IGameState gameState)
+        {
+            var result = new GameResult
+            {
+                WinProgress = (gameState.Population / Goal),
+                LoseProgress = (gameState.Turn) / (float)TurnLimit
+            };
+
+            if (result.WinProgress > 1)
+                result.WinProgress = 1;
+
+            if (gameState.Money >= Goal)
+                return result.Win();
+
+            if (gameState.Turn > TurnLimit - 1) // we subtract 1 bcs check happens at the end of the turn which means
+                // otherwise it will always be late by one round
+                return result.Lose();
+
+            return result.Continue();
+        }
+    }
+}
